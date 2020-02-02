c7c19f5b8f7fe8d308dab9d976abafadadd60066
matheuscmacedo0 <matheuscmacedo@hotmail.com>
2020-02-02 13:18:35 -0300

    Exercício número primo

---
+++ prime-number.js
@@ -0,0 +1,14 @@
+// Dado um número, retorne se o mesmo é primo ou não
+
+//Peneira de Eratóstenes
+
+function isPrime(num) {
+
+    return num == 2 ? true : "";
+
+    for (let i = 2; i <= num; i+2) {
+        for (let j = 3; j <= num; j+3)
+
+    }
+
+    }
